84334daa4bc06cab846febdbb75823e73278d1ee
ila36IX <jbariali002@gmail.com>
2023-10-15 02:55:38 +0000

    Fix the function insert_dnodeint_at_index

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,8 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    if (type(roman_string) != str || roman_string == None)
-    return (0)
+    if (type(roman_string) != str || roman_string == None):
+        return (0)
     equiled_digit = 0
     units = {
         'VIII': 8,

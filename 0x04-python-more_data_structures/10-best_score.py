f93469542ac43fe9d14fb6df1ef361976c853197
ila36IX <jbariali002@gmail.com>
2023-10-15 18:36:00 +0000

    Fix the best_score function

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if (a_dictionary == None):
-        return None;
-    big_value = 0;
-    key = "";
-    for i,j in a_dictionary.items():
+    if (a_dictionary is None):
+        return None
+    if (len(a_dictionary.items()) == 0):
+        return None
+    big_value = 0
+    key = ""
+    for i, j in a_dictionary.items():
         if big_value < j:
             big_value = j
             key = i

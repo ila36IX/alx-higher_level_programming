80e545f052b5b0b46b46273716f667ff2cbece73
ila36IX <jbariali002@gmail.com>
2023-10-15 02:51:27 +0000

    Fix the function insert_dnodeint_at_index

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,65 +4,62 @@
 def roman_to_int(roman_string):
     equiled_digit = 0
     units = {
-        'I': 1,
-        'II': 2,
-        'III': 3,
-        'IV': 4,
-        'V': 5,
-        'VI': 6,
-        'VII': 7,
         'VIII': 8,
-        'IX': 9
+        'VII': 7,
+        'III': 3,
+        'IX': 9,
+        'VI': 6,
+        'IV': 4,
+        'II': 2,
+        'V': 5,
+        'I': 1
     }
     tens = {
-        'X' : 10,
-        'XX': 20,
-        'XXX': 30,
-        'XL': 40,
-        'L': 50,
-        'LX': 60,
-        'LXX': 70,
         'LXXX': 80,
-        'XC': 90
+        'LXX': 70,
+        'XXX': 30,
+        'XC': 90,
+        'LX': 60,
+        'XL': 40,
+        'XX': 20,
+        'L': 50,
+        'X': 10
     }
     Hundreds = {
-        'C': 100,
-        'CC': 200,
-        'CCC': 300,
-        'CD': 400,
-        'D': 500,
-        'DC': 600,
-        'DCC': 700,
         'DCCC': 800,
-        'CM': 900
+        'CCC': 300,
+        'DCC': 700,
+        'CM': 900,
+        'DC': 600,
+        'CD': 400,
+        'CC': 200,
+        'D': 500,
+        'C': 100
     }
     Thousands = {
-        'M': 1000,
+        'MMM': 3000,
         'MM': 2000,
-        'MMM': 3000
+        'M': 1000
     }
 
-    for rom, dig in reversed(sorted(units.items(), key=lambda x: len(x[0]))):
-        if rom in roman_string:
-            equiled_digit += dig
-            roman_string = roman_string[:-len(rom)]
-            break;
-    for rom, dig in reversed(sorted(tens.items(), key=lambda x: len(x[0]))):
+    for rom, dig in units.items():
         if rom in roman_string:
             equiled_digit += dig
             roman_string = roman_string[:-len(rom)]
             break
-    for rom, dig in reversed(sorted(Hundreds.items(), key=lambda x: len(x[0]))):
+    for rom, dig in tens.items():
         if rom in roman_string:
             equiled_digit += dig
             roman_string = roman_string[:-len(rom)]
             break
-    for rom, dig in reversed(sorted(Thousands.items(), key=lambda x: len(x[0]))):
+    for rom, dig in Hundreds.items():
+        if rom in roman_string:
+            equiled_digit += dig
+            roman_string = roman_string[:-len(rom)]
+            break
+    for rom, dig in Thousands.items():
         if rom in roman_string:
             equiled_digit += dig
             roman_string = roman_string[:-len(rom)]
             break
     return (equiled_digit)
-
-print(roman_to_int("XXXIX"))
-

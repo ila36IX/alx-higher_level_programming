d25d53822f48a4c85a4c44ea657a32aeb4dec9b3
ila36IX <jbariali002@gmail.com>
2023-10-15 18:19:40 +0000

    Fix the update_dictionery function

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-def print_sorted_dictionary(a_dictionary):
-    for i, j in sorted(a_dictionary.items(), key=lambda x: x[0]):
-        print("{}: {}".format(i, j))
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+    return (a_dictionary)

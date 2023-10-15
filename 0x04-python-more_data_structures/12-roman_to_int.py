cdcb9ce85b4ebd4aa4ee0a0c46e1619072c90004
ila36IX <jbariali002@gmail.com>
2023-10-15 03:05:39 +0000

    Fix roman to integer function

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 
 def roman_to_int(roman_string):
-    if (type(roman_string) != str and roman_string == None):
+    if (isinstance(roman_string, str) or roman_string is None):
         return (0)
     equiled_digit = 0
     units = {

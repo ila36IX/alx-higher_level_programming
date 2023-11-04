ca654504bafa7f676b80e9692a0e96e6e42047f9
ila36IX <jbariali002@gmail.com>
2023-11-04 01:40:47 +0000

    💥 c is fun

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,25 +1,16 @@
 #include <Python.h>
 #include <object.h>
 #include <listobject.h>
+#include <string.h>
 
-void print_python_list(PyObject *p)
-{
-	long int i;
-	PyVarObject *o = (PyVarObject *) p;
-	PyListObject *obj = (PyListObject *) p;
-	long int size = o->ob_size;
-	const char *type_str = NULL;
-	
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", obj->allocated);
-	for (i = 0; i < size; i++)
-	{
-		type_str = ((obj->ob_item[i])->ob_type)->tp_name;
-		printf("Element %ld: %s\n", i, type_str);
-	}
-}
+#define ngtf(n) ((n >= 0) ? (n) : (n + 0xff + 1))
 
+/**
+ * print_python_bytes - Print python bytes infos
+ *
+ * @p: Bytes object
+ * Return: Nothing
+ */
 void print_python_bytes(PyObject *p)
 {
 	char *str;
@@ -30,10 +21,9 @@ void print_python_bytes(PyObject *p)
 	if (!PyBytes_Check(p))
 	{
 		printf("[.] bytes object info\n");
- 	 	printf("  [ERROR] Invalid Bytes Object\n");
+		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-
 	str = PyBytes_AsString(p);
 	size = PyBytes_Size(p);
 	printf("[.] bytes object info\n");
@@ -47,7 +37,33 @@ void print_python_bytes(PyObject *p)
 	for (i = 0; i < first_chars; i++)
 	{
 		printf(" ");
-		printf("%02x", str[i]);
+		printf("%02x", ngtf(str[i]));
 	}
 	printf("\n");
+}
+
+/**
+ * print_python_list - Print list details
+ *
+ * @p: List object
+ * Return: Nothing
+ */
+void print_python_list(PyObject *p)
+{
+	long int i;
+	PyVarObject *o = (PyVarObject *) p;
+	PyListObject *obj = (PyListObject *) p;
+	long int size = o->ob_size;
+	const char *type_str = NULL;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", size);
+	printf("[*] Allocated = %ld\n", obj->allocated);
+	for (i = 0; i < size; i++)
+	{
+		type_str = ((obj->ob_item[i])->ob_type)->tp_name;
+		printf("Element %ld: %s\n", i, type_str);
+		if (PyBytes_Check(obj->ob_item[i]))
+			print_python_bytes(obj->ob_item[i]);
+	}
 }

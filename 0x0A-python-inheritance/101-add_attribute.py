6463a8c185f7806440d4d169168821c73ead3817
ila36IX <jbariali002@gmail.com>
2023-11-26 02:08:19 +0000

    🧰 Python - Inheritance

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 
 def add_attribute(instance, prop_name, value):
     """Add new attrebute to a class using string"""
-    if not hasattr(instance, "__dict__"):
+    if hasattr(instance, "__dict__"):
         setattr(instance, prop_name, value)
     else:
         raise TypeError("can't add new attribute")

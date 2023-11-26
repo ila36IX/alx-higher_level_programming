551f4261943a6209307969d89602cf68f13169dc
ila36IX <jbariali002@gmail.com>
2023-11-26 00:15:07 +0000

    🧰 Python - Inheritance

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+ #!/usr/bin/python3
 
 def is_kind_of_class(obj, a_class):
     """Check if is instance or a subclass"""
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,4 @@
-#!usr/bin/python3
+ #!/usr/bin/python3
 
 def inherits_from(obj, a_class):
     """Check if object is a subclass of other class"""

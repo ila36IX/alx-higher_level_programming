34d5c8b21ccedb7eab5ab6c4002940adf38f692d
It-Ayla <ista.alay23@gmail.com>
2023-12-02 13:08:00 +0100

    Full project

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,16 +7,8 @@ def safe_print_list(my_list=[], x=0):
             x -= 1
             print(my_list[many_chars], end="")
             many_chars += 1
-    except:
-        pass
+    except Exception as err:
+        print()
+        return many_chars
     print()
     return many_chars
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))

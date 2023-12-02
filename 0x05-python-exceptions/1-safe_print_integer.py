1914c0d7aea476357435699d4a95cff2ed5c82f0
It-Ayla <ista.alay23@gmail.com>
2023-12-02 13:14:05 +0100

    Full project

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as e:
+        return False
+    return True
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    char_number = 0
+    try:
+        while x != 0:
+m            x 

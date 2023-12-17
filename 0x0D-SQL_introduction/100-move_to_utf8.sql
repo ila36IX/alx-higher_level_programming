710e12ba4f7ddf7bcd24772d730818b2007b002e
ila36IX <jbariali002@gmail.com>
2023-12-17 06:20:07 +0100

    🛢️ Last task query

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -9,11 +9,12 @@ USE hbtn_0c_0;
 
 -- Specifying the Table characterset
 ALTER TABLE first_table
+CONVERT TO
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Specifying the Column characterset
-ALTER TABLE first_table MODIFY
-name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+-- ALTER TABLE first_table MODIFY
+-- name VARCHAR(256)
+-- CHARACTER SET utf8mb4
+-- COLLATE utf8mb4_unicode_ci;
--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -3,5 +3,4 @@
 SELECT state, MAX(value) AS max_temp
 FROM temperatures
 GROUP BY state
-ORDER BY max_temp
-LIMIT 10;
+ORDER BY state;

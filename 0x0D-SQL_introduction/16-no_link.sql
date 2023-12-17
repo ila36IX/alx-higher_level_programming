dce5eeedf0a1476c9bc0a1d47c8d2ad0ada08214
ila36IX <jbariali002@gmail.com>
2023-12-17 04:45:38 +0100

    git please work

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,5 @@
 -- lists all records of the table second_table
 -- Don’t list rows without a name value
 SELECT score, name FROM second_table
-WHERE name IS NOT NULL;
+WHERE name IS NOT NULL
+ORDER BY score;

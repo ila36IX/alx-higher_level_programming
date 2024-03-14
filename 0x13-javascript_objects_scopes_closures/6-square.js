a772ad2ba028b169d7dc994fdd86b7fd041bc33e
ila36IX <jbariali002@gmail.com>
2024-03-14 21:38:40 +0000

    JS is compiled language

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const square = require('./5-square');
 
 class Square extends square {
-  charPrint (c = 'x') {
+  charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
     }

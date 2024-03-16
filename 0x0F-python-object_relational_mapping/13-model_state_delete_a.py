d50ecefc1207e3f3d683373ffbe36ec906a84e07
ila36IX <jbariali002@gmail.com>
2024-03-16 19:57:58 +0000

    Trying to kick the errors

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = session.query(State)
-    .filter(State.name.like("%a%"))
-    .delete()
+    states_to_delete = session.query(State).\
+        filter(State.name.like("%a%")).\
+        delete()
 
     session.commit()

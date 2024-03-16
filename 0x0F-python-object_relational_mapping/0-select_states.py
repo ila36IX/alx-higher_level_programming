657243b98a312bb4e5b28bda86a5620c52d6c2cc
ila36IX <jbariali002@gmail.com>
2024-03-16 06:54:01 +0000

    Trying to kick the errors

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,32 +2,21 @@
 
 """
 
-testing
-testing
-testing
-testing
+Print the row of a table using MySQLdb
 
 """
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    """
-
-    testing
-    testing
-    testing
-    testing
-
-    """
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost",
                          user=username,
-                         passwd=password,
-                         db=db_name,
+                         password=password,
+                         database=db_name,
                          port=3306)
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id")

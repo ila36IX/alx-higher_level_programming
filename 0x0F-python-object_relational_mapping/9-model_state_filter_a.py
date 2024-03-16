0fc1e5fa6c46f251fb016a5ef96851ffac10c671
ila36IX <jbariali002@gmail.com>
2024-03-16 07:46:47 +0000

    Trying to kick the errors

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -6,13 +6,13 @@ Lists all State objects that contain the letter 'a'
 
 """
 
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 if __name__ == "__main__":
-    db_str = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}"
+    db_str = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}"
     engine = create_engine(db_str)
 
     Base.metadata.create_all(engine)
@@ -20,7 +20,9 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    rows = session.query(State).filter(State.name.like("%a%")).order_by(State.id)
+    rows = session.query(State).\
+        filter(State.name.like("%a%")).\
+        order_by(State.id)
 
     for row in rows:
         print(row.id)

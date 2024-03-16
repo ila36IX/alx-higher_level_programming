ea6358ac73cfd5609d682233052290d14c984bf6
ila36IX <jbariali002@gmail.com>
2024-03-16 20:51:20 +0000

    Trying to kick the errors

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,21 +7,24 @@ sqlalchemy
 """
 
 from sys import argv
-from model_state import Base, State 
+from model_state import Base, State
 from model_city import City
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-if __name__ == "__main__": 
-    db_str = f"mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}"
+if __name__ == "__main__":
+    db_str = "mysql+mysqldb://{}:{}@localhost:3306/{}".\
+        format(argv[1], argv[2], argv[3])
     engine = create_engine(db_str)
 
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    result = session.query(State, City).join(City, City.state_id == State.id).order_by(City.id).all()
+
+    result = session.query(State, City).\
+        join(City, City.state_id == State.id).\
+        order_by(City.id).all()
 
     for state, city in result:
         print(f"{state.name}: ({city.id}) {city.name}")

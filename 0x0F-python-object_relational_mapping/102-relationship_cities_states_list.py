cf06cf82c1c7a01ab7112e0f159861875336e082
ila36IX <jbariali002@gmail.com>
2024-03-17 07:30:20 +0000

    Trying to kick the errors

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -22,4 +22,4 @@ if __name__ == "__main__":
 
     cities = session.query(City).order_by(City.id).all()
     for city in cities:
-        print("{}: {} -> {}".format(city.id, city.name, city.state))
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))

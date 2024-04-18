9d8dbc6f1f915541b5fefcecb0ca6a1713229b18
ila36IX <jbariali002@gmail.com>
2024-04-19 00:43:36 +0100

    sends a POST request to http://0.0.0.0:5000/search_user

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
             print("No result")
         else:
             print(f"[{json_r.get('id')}] {json_r.get('name')}")
-    except Expression as e:
+    except Exception as e:
         print("Not a valid JSON")

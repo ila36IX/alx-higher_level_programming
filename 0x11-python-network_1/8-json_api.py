c11207dca4ccac112b37b01b8cbced07c84056d5
ila36IX <jbariali002@gmail.com>
2024-04-19 00:29:22 +0100

    sends a POST request to http://0.0.0.0:5000/search_user

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -11,16 +11,14 @@ if __name__ == "__main__":
     post_letter = ""
 
     if len(argv) == 2:
-        post_letter = argv[2]
-    r = requests.data("http://0.0.0.0:5000/search_user", data={"q": post_letter})
+        post_letter = argv[1]
+    r = requests.post("http://0.0.0.0:5000/search_user", data={"q": post_letter})
 
     try:
         json_r = r.json()
         if len(json_r) == 0:
             print("No result")
         else:
-            for k, v in json_r:
-                print(f"[{k}] {v}")
+                print(f"[{json_r.get('id')}] {json_r.get('name')}")
     except expression as e:
         print("Not a valid JSON")
-

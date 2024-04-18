c89f3cc87956b6d8ffbbf45c4011aec05b533a1b
ila36IX <jbariali002@gmail.com>
2024-04-19 00:57:30 +0100

    Find a peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-# finds a peak in a list of unsorted integers
+"""
+Finds a peak in a list of unsorted integers
+"""
+
 def find_peak(list_of_integers):
     if type(list_of_integers) is not list:
         return None
@@ -15,11 +18,3 @@ def find_peak(list_of_integers):
             l = m + 1
         else:
             return num[m]
-
-
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))

3e595f104b6ac944e18e44a8d2769caafedf6543
ila36IX <jbariali002@gmail.com>
2024-04-27 08:54:26 +0100

    synchronous javascritp weird stuff

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,7 +4,7 @@ const request = require('request');
 const { argv } = require('node:process');
 const util = require('node:util');
 
-const rp = util.promisify(request)
+const rp = util.promisify(request);
 
 const id = argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
@@ -12,7 +12,7 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}/`;
 function getCharacterName (urls, index) {
   if (!urls || index === urls.length) { return; }
   rp(urls[index])
-		.then(data => data.body)
+    .then(data => data.body)
     .then((data) => JSON.parse(data))
     .then((characterInfo) => console.log(characterInfo.name))
     .then(() => getCharacterName(urls, ++index))
@@ -21,9 +21,9 @@ function getCharacterName (urls, index) {
 
 async function getAllCharacters () {
   const characters = await rp(url)
-		.then(data => data.body)
+    .then(data => data.body)
     .then((data) => JSON.parse(data).characters)
-    .catch((err) => console.log('Not exists:'));
+    .catch((err) => console.log(err));
   getCharacterName(characters, 0);
 }
 getAllCharacters();
